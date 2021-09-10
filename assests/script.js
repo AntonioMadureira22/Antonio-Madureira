8910989c1411b749753833ea7e84a9695836bfbc
Antonio Madureira <antoniomadureira2222@gmail.com>
2021-09-09 22:07:42 -0400

    added more materialize css to hero img

--- assests/script.js
+++ assests/script.js
@@ -1,2 +1,10 @@
-
+//jquery for mobile side navbar
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+       
+//jquery for hero image
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
        
